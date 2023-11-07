e74d53bdb461d4e785cf701613aec50d36525701
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-11-07 22:40:25 +0700

    [ADD] add header banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import Head from 'next/head'
-import { Open_Sans } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 
-const openSans = Open_Sans({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ['latin'], weight: '400' })
 
 export default function Home() {
 
@@ -12,23 +13,38 @@ export default function Home() {
   }, [])
 
   return (
-    <div id='page-home' className={openSans.className}>
+    <div id='page-home' className={poppins.className}>
 
       <Head>
         <title>
           Home | Peworld
         </title>
       </Head>
-      
-      <Navbar/>
 
-      <header className='container mx-auto'>
-      
-        
-        
-      </header>
+      <Navbar />
 
-      <main>
+      <main className='container mx-auto mt-[15vh]'>
+        
+        <section className='flex justify-evenly mb-[5vh] flex-wrap'>
+
+          <div className='flex items-center w-[50vh]'>
+            <div>
+              <h1>
+                Talenta terbaik negri untuk perubahan revolusi 4.0                                                       
+              </h1>
+              <p className='my-8'>Recruit talenta terbaik sesuai dengan kebutuhan industri.</p>
+              <button id='banner-button'>
+                Mulai Sekarang
+              </button>
+            </div>
+          </div>
+
+          <div id='bannerImage' className='w-[50vh]'>
+            <img src='/assets/images/banner.png' alt='banner'></img>
+          </div>
+
+        </section>
+
       </main>
 
     </div>
