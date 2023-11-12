a677ed7f729da91efce88d3850e131ba8c53f512
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-11-12 23:32:20 +0700

    [f] fix margin on mobile

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -42,9 +42,9 @@ export default function Home() {
 
       <main className='container mx-auto lg:mt-[12vh] max-sm:w-[88%]'>
 
+     
         {/* hero section */}
-        <section id='hero-section' className='scroll-helper grid grid-cols-2 max-md:grid-cols-1 mb-16 mt-36'>
-
+        <section id='hero-section' className='scroll-helper grid grid-cols-2 max-md:grid-cols-1 mb-16 max-md:mt-24 mt-36'>
           <div className='flex items-center justify-self-center max-lg:items-center max-md:w-[80vw] lg:max-w-[50vw] xl:max-w-[28vw] p-3'>
             <div className=''>
               <h1 className=' max-sm:text-[5.5vw] max-md:text-[5vw] text-[33px] font-bold'>
