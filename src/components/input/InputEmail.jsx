95621383ea16a646b6a481698e790b0fe2f5b4e0
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-11-15 10:14:26 +0700

    [+] add component input email

---
+++ src/components/input/InputEmail.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function InputEmail({id, labelName, placeholder, onChange}) {
+  return (
+	<div className='flex flex-col gap-2' 
+	onChange={onChange}>
+		<label htmlFor={id ?? 'email'}>{labelName ?? 'Email'}</label>
+		<input id={id ?? 'email'} type="email" placeholder={placeholder ?? 'Insert Email Address'}
+		className='focus:outline-none border-pw-gray20 border-2 rounded-md w-full p-2 h-10' />
+	</div>
+  )
+}
