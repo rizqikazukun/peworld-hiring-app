6e08a99cac73309b633516f58c788cc815fa5173
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-11-15 22:27:10 +0700

    🔷 update input text component

--- src/components/input/InputText.jsx
+++ src/components/input/InputText.jsx
@@ -5,7 +5,7 @@ export default function InputText({id, labelName, placeholder, onChange}) {
 	<div className='flex flex-col gap-2' 
 	onChange={onChange}>
 		<label htmlFor={id}>{labelName}</label>
-		<input id={id} type="text" placeholder={placeholder}
+		<input id={id} type="text" placeholder={placeholder ? placeholder : `Insert ${labelName}`}
 		className='focus:outline-none border-pw-gray20 border-2 rounded-md w-full p-2 h-10' />
 	</div>
   )
