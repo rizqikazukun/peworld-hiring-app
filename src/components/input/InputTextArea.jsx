b885e976f161c0150b80d22dd8f255459ad03021
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-11-15 22:26:18 +0700

    ✅ adding component Input Text area

---
+++ src/components/input/InputTextArea.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function InputTextArea({id, labelName, rows, placeholder, onChange}) {
+  return (
+	<div className='flex flex-col gap-2' 
+	onChange={onChange}>
+		<label htmlFor={id}>{labelName}</label>
+		<textarea id={id} rows={rows ? rows : 4} placeholder={placeholder ? placeholder : `Insert ${labelName}`}
+		className='focus:outline-none border-pw-gray20 border-2 rounded-md w-full p-2 h-10' />
+	</div>
+  )
+}
