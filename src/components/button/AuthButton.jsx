9a24a897da286e92f190e6f6c7d6eb365a0f334f
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-11-17 23:10:38 +0700

    ✅ add Component AuthButton

---
+++ src/components/button/AuthButton.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react'
+
+export default function AuthButton({ type, onClick, loadingIndicator }) {
+	return (
+		<button className=' bg-pw-orange hover:bg-pw-orange-hover h-10 rounded-lg'
+			onClick={onClick}>
+			{
+				loadingIndicator ?
+					<p className='text-white font-semibold'>
+						<img className='inline h-[20px] animate-spin'
+							src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Vector_Loading.svg"
+							style={{ filter: 'brightness(0) saturate(100%) invert(98%) sepia(4%) saturate(2%) hue-rotate(237deg) brightness(111%) contrast(100%)' }}
+							alt="loading" /> Loading...</p> :
+					<span className='text-white font-semibold'>{ String(type).toLocaleLowerCase() === 'login' ? 'login' : 'register'}</span>
+			}
+		</button>
+	)
+}
