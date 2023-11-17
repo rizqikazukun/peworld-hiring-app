b55698d764a80c6d46c158d61a6496b360faa726
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-11-18 00:37:51 +0700

    ✅ add conditional rendering on navbar

--- src/components/MobileNavbarLink.jsx
+++ src/components/MobileNavbarLink.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react'
 
-export default function MobileNavbarLink({ onClick, pageName }) {
+export default function MobileNavbarLink({ onClick, pageName, hidden }) {
 	return (
 		<div className={`border-b-2 py-4 w-full flex hover:cursor-pointer ${pageName.toLowerCase() === 'close' ? 'justify-center' : 'justify-between'}`}
-			onClick={onClick}>
+			onClick={onClick} hidden={hidden}>
 			<p className=' text-[2.6vh] font-bold text-pw-purple'>
 				{pageName}
 			</p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,25 @@ import React from 'react'
 import { Poppins } from 'next/font/google'
 import { Router, useRouter } from 'next/router'
 import MobileNavbarLink from './MobileNavbarLink'
+import { deleteCookie, getCookie, hasCookie } from 'cookies-next'
 const poppins = Poppins({ subsets: ['latin'], weight: '400' })
 
 export default function Navbar() {
 	const [expand, setExpand] = React.useState(false)
 	const router = useRouter()
 
+	const [user, setUser] = React.useState(undefined)
+
+	const isHidden = hasCookie('user') && hasCookie('token')
+
+
 	React.useEffect(() => {
 
-	})
+		if (hasCookie('user')) {
+			setUser(JSON.parse(getCookie('user')))
+		}
+
+	}, [])
 
 	return (
 		<div className={poppins.className}>
@@ -37,17 +47,27 @@ export default function Navbar() {
 
 					<div id='nav-item-right' className='flex flex-row gap-2 items-center'>
 
-						<Link href='/login'>
-							<button className='w-[128px] rounded-full px-1 h-10 max-lg:hidden bg-white hover:bg-pw-white-hover border-pw-purple border-2'>
-								<span className=' text-pw-purple font-semibold'>Login</span>
-							</button>
-						</Link>
+						{
+							!isHidden ? '' :
+								<button className='flex items-center gap-5 ps-2 w-fit h-12 rounded-full max-lg:hidden bg-white text-pw-purple hover:text-pw-purple-hover'
+									onClick={() => {
+										deleteCookie('user')
+										deleteCookie('token')
+										setUser(undefined)
+									}}
+								>
+									Logout <img className='h-full object-cover object-top rounded-full' src='/assets/icons/profile.svg' alt="" />
+								</button>
+						}
 
-						<Link href='/register'>
-							<button className='w-[128px] rounded-full px-1 h-10 max-lg:hidden  bg-pw-purple hover:bg-pw-purple-hover border-pw-purple border-2'>
-								<span className=' text-white font-semibold'>Register</span>
-							</button>
-						</Link>
+						<button className='w-[128px] rounded-full px-1 h-10 max-lg:hidden bg-white hover:bg-pw-white-hover border-pw-purple border-2'
+							onClick={() => router.push('/login')} hidden={isHidden}>
+							<span className=' text-pw-purple font-semibold'>Login</span>
+						</button>
+
+						<button className='w-[128px] rounded-full px-1 h-10 max-lg:hidden  bg-pw-purple hover:bg-pw-purple-hover border-pw-purple border-2' onClick={() => router.push('/register')} hidden={isHidden}>
+							<span className=' text-white font-semibold'>Register</span>
+						</button>
 
 						<button className='max-lg:flex hidden menu-button h-10 w-10'
 							onClick={() => {
@@ -58,7 +78,12 @@ export default function Navbar() {
 								}
 
 							}}>
-							<img className='m-auto w-full' src="/burger-menu.svg" alt="menu" />
+
+							{
+								user !== undefined ? <img className='m-auto w-full rounded-full' src='/assets/icons/profile.svg' alt="menu" /> :
+									<img className='m-auto w-full' src="/burger-menu.svg" alt="menu" />
+							}
+
 						</button>
 
 					</div>
@@ -81,15 +106,31 @@ export default function Navbar() {
 								router.push('/talents')
 							}} />
 
-							<MobileNavbarLink pageName='Login' onClick={() => {
-								setExpand(false)
-								router.push('/login')
-							}} />
+							{
+								isHidden ? "" :
+									<MobileNavbarLink pageName='Login' onClick={() => {
+										setExpand(false)
+										router.push('/login')
+									}} />
+							}
 
-							<MobileNavbarLink pageName='Register' onClick={() => {
-								setExpand(false)
-								router.push('/register')
-							}} />
+							{
+								isHidden ? "" :
+									<MobileNavbarLink pageName='Register' onClick={() => {
+										setExpand(false)
+										router.push('/register')
+									}} />
+							}
+
+							{
+								!isHidden ? "" :
+									<MobileNavbarLink pageName='Logout' onClick={() => {
+										deleteCookie('user')
+										deleteCookie('token')
+										setUser(undefined)
+										setExpand(false)
+									}} />
+							}
 
 							<MobileNavbarLink pageName='Close' onClick={() => {
 								setExpand(false)
