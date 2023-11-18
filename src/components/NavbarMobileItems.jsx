dcadcde11dfadb0e72db1b64aaa47dc61b34dcd9
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-11-18 11:10:10 +0700

    🔷 rename component name

--- src/components/NavbarMobileItems.jsx
+++ src/components/NavbarMobileItems.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react'
 
-export default function MobileNavbarLink({ onClick, pageName, hidden }) {
+export default function NavbarMobileItems({ onClick, pageName }) {
 	return (
 		<div className={`border-b-2 py-4 w-full flex hover:cursor-pointer ${pageName.toLowerCase() === 'close' ? 'justify-center' : 'justify-between'}`}
-			onClick={onClick} hidden={hidden}>
+			onClick={onClick}>
 			<p className=' text-[2.6vh] font-bold text-pw-purple'>
 				{pageName}
 			</p>
