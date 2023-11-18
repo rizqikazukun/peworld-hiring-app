89e99b00c6f9589adc7e1845da361bd0e3720c98
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-11-18 19:59:02 +0700

    ✅ add defaultValue to some Input Component

--- src/components/input/InputText.jsx
+++ src/components/input/InputText.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function InputText({id, labelName, placeholder, onChange}) {
+export default function InputText({id, labelName, placeholder, onChange, defaultValue}) {
   return (
 	<div className='flex flex-col gap-2' 
 	onChange={onChange}>
 		<label htmlFor={id}>{labelName}</label>
-		<input id={id} type="text" placeholder={placeholder ? placeholder : `Insert ${labelName}`}
+		<input id={id} type="text" defaultValue={defaultValue} placeholder={placeholder ? placeholder : `Insert ${labelName}`}
 		className='focus:outline-none border-pw-gray20 border-2 rounded-md w-full p-2 h-10' />
 	</div>
   )
--- src/components/input/InputTextArea.jsx
+++ src/components/input/InputTextArea.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function InputTextArea({id, labelName, rows, placeholder, onChange}) {
+export default function InputTextArea({id, labelName, rows, placeholder, onChange, defaultValue}) {
   return (
 	<div className='flex flex-col gap-2' 
 	onChange={onChange}>
 		<label htmlFor={id}>{labelName}</label>
-		<textarea id={id} rows={rows ? rows : 4} placeholder={placeholder ? placeholder : `Insert ${labelName}`}
+		<textarea id={id} rows={rows ? rows : 4} spellCheck='false' defaultValue={defaultValue} placeholder={placeholder ? placeholder : `Insert ${labelName}`}
 		className='focus:outline-none border-pw-gray20 border-2 rounded-md w-full p-2' />
 	</div>
   )
